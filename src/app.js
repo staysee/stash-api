66f6daf33a65d02afe2b5a855b70fd8e06826520
Stacey Sugiono <sugiono.stacey@gmail.com>
2020-11-16 20:55:50 -0800

    update cors and client origin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { CLIENT_ORIGIN, NODE_ENV } = require('./config')
 
 const recipesRouter = require('./recipes/recipes-router')
 const mealsRouter = require('./meals/meals-router')
@@ -18,7 +18,9 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(cors({
+    origin: CLIENT_ORIGIN
+}))
 app.use(express.json())
 
 app.use('/api/recipes', recipesRouter)
--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    CLIENT_ORIGIN: 'http://localhost:3000',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql:stash_admin@localhost/stash',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql:stash_admin@localhost/stash-test',
     JWT_SECRET: process.env.JWT_SECRET || 'secret-stash-secret'
